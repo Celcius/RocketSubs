dd2c3eafa12d552476c1c2a5cb44489898fed3e9
Ivo Capelo <ivo.capelo@gmail.com>
2020-06-10 15:45:51 +0100

    Simple Game Mode Controller

---
+++ GameJoltApiTest/Assets/Refactored/Scripts/Gameplay/GameMode.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using AmoaebaUtils;
+
+public class GameMode : ScriptableObject
+{
+    [SerializeField]
+    private SceneVar sceneVar;
+    protected bool isRunning = false;
+
+    private void OnEnable()
+    {
+        StopGameMode();
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    protected Scene GetScene()
+    {
+        return SceneManager.GetSceneByBuildIndex(sceneVar.Value.handle);
+    }
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        Scene actualScene = GetScene();
+        
+        if(actualScene != null && scene.name.CompareTo(actualScene.name) == 0)
+        {
+            StartGameMode();
+        }
+        else if(isRunning)
+        {
+            StopGameMode();
+        }
+    }
+
+    public void PlayGameMode()
+    {
+        SceneManager.LoadScene(sceneVar.Value.name, LoadSceneMode.Single);
+    }
+    
+    public virtual void StopGameMode()
+    {
+        if(!isRunning)
+        {
+            return;
+        }
+        isRunning = false;
+    }
+    public virtual void StartGameMode()
+    {
+        if(isRunning)
+        {
+            return;
+        }
+        isRunning = true;
+    }
+}
---
+++ GameJoltApiTest/Assets/Refactored/Scripts/Gameplay/PlayGameMode.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGameMode : GameMode
+{
+    
+    [SerializeField]
+    private BoolVar isInputBlocked;
+
+    [SerializeField]
+    protected FloatVar fuel;
+
+    [SerializeField]
+    protected FloatVar health;
+
+    [SerializeField]
+    protected FloatVar maxHealth;
+
+    [SerializeField]
+    protected IntVar skipCount;
+
+    [SerializeField]
+    protected BoolVar usingFuel;
+
+    [SerializeField]
+    protected BoolVar isPlaying;
+
+    [SerializeField]
+    protected Transform endGameMenu;
+
+    public override void StartGameMode()
+    {
+        if(isRunning)
+        {
+            return;
+        }
+        
+        fuel.Reset();
+        health.Value = maxHealth.Value;
+        skipCount.Value = 0;
+        usingFuel.Value = false;
+
+        health.OnChange += OnHealthChanged;
+
+        isPlaying.Value = isRunning = true;
+        isInputBlocked.Value = false;
+    }
+
+    public override void StopGameMode()
+    {
+        if(!isRunning)
+        {
+            return;
+        }
+        
+        health.OnChange -= OnHealthChanged;
+
+        isPlaying.Value = isRunning = false;
+        isInputBlocked.Value = true;
+    }
+
+    private void OnHealthChanged(float oldVal, float newVal)
+    {
+        if(isRunning && oldVal > 0 && newVal <= 0)
+        {
+            FinishedGameMode();
+        }
+    }
+
+    private void FinishedGameMode()
+    {
+        StopGameMode();
+        Instantiate(endGameMenu);
+    }
+}
